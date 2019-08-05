18a8b8266b3e4d9df37a17965746f70f3e45c626
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-08-05 15:51:20 -0700

    add some processing direct from s3

--- goes_viewer/process_files.py
+++ goes_viewer/process_files.py
@@ -1,32 +1,42 @@
 import datetime as dt
+
+
+import boto3
 import numpy as np
 from PIL import Image
 from pyproj import CRS, transform
 import pyresample
+import s3fs
 import xarray as xr
 
 
-from goes_viewer.constants import CONTRAST, G16_CORNERS, G17_CORNERS, WEB_MERCATOR, DX, DY
+from goes_viewer.constants import (
+    CONTRAST,
+    G16_CORNERS,
+    G17_CORNERS,
+    WEB_MERCATOR,
+    DX,
+    DY,
+)
 
 
-def open_file(path, corners):
-    ds = xr.open_dataset(path, engine="netcdf4")
+def open_file(path, corners, engine='netcdf4'):
+    ds = xr.open_dataset(path, engine=engine)
     proj_info = ds.goes_imager_projection
     proj4_params = {
         "ellps": "WGS84",
-        "a": proj_info.semi_major_axis,
-        "b": proj_info.semi_minor_axis,
-        "rf": proj_info.inverse_flattening,
+        "a": proj_info.semi_major_axis.item(),
+        "b": proj_info.semi_minor_axis.item(),
+        "rf": proj_info.inverse_flattening.item(),
         "proj": "geos",
-        "lon_0": proj_info.longitude_of_projection_origin,
+        "lon_0": proj_info.longitude_of_projection_origin.item(),
         "lat_0": 0.0,
-        "h": proj_info.perspective_point_height,
+        "h": proj_info.perspective_point_height.item(),
         "x_0": 0,
         "y_0": 0,
         "units": "m",
         "sweep": proj_info.sweep_angle_axis,
     }
-
     crs = CRS.from_dict(proj4_params)
     bnds = transform(crs.geodetic_crs, crs, corners[:, 0], corners[:, 1])
     ds = ds.update(
@@ -100,7 +110,9 @@ def make_resample_params(ds, corners):
             ds.y.max().item(),
         ),
     )
-    pts = transform(ds.crs.geodetic_crs, WEB_MERCATOR, corners[:, 0], corners[:, 1])
+    pts = transform(
+        ds.crs.geodetic_crs, WEB_MERCATOR, sorted(corners[:, 0]), sorted(corners[:, 1])
+    )
     width = int((pts[0][1] - pts[0][0]) / DX)
     height = int((pts[1][1] - pts[1][0]) / DY)
     webm_area = pyresample.AreaDefinition(
@@ -113,7 +125,10 @@ def make_resample_params(ds, corners):
         area_extent=(pts[0][0], pts[1][0], pts[0][1], pts[1][1]),
     )
     shape = (height, width)
-    return pyresample.bilinear.get_bil_info(goes_area, webm_area, 6e3, neighbours=8), shape
+    return (
+        pyresample.bilinear.get_bil_info(goes_area, webm_area, 6e3, neighbours=8),
+        shape,
+    )
 
 
 def resample_image(resample_params, shape, img_arr):
@@ -134,15 +149,64 @@ def make_img_filename(ds):
     return f'figs/{ds.platform_ID}_{date.strftime("%Y-%m-%dT%H:%M:%SZ")}.png'
 
 
+def get_s3_keys(bucket, timestamp=None, prefix="ABL-L2-MCMIPF"):
+    """
+    Generate the keys in an S3 bucket.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    """
+    s3 = boto3.client("s3")
+    kwargs = {"Bucket": bucket}
+
+    kwargs["Prefix"] = prefix
+    if timestamp is not None:
+        kwargs['Prefix'] += timestamp.strftime('%Y/%j/%H')
+
+    while True:
+        resp = s3.list_objects_v2(**kwargs)
+        if resp["KeyCount"] == 0:
+            break
+        for obj in resp["Contents"]:
+            key = obj["Key"]
+            if key.startswith(prefix):
+                yield key
+
+        try:
+            kwargs["ContinuationToken"] = resp["NextContinuationToken"]
+        except KeyError:
+            break
+
+
+def process_s3_file(bucket, key):
+    fs = s3fs.S3FileSystem(anon=True)
+    remote_file = fs.open(f'{bucket}/{key}', mode='rb')
+    if 'goes17' in bucket:
+        corners = G17_CORNERS
+    else:
+        corners = G16_CORNERS
+    ds = open_file(remote_file, corners, 'h5netcdf')
+    img = make_geocolor_image(ds)
+    resample_params, shape = make_resample_params(ds, G17_CORNERS)
+    nimg = resample_image(resample_params, shape, img)
+    Image.fromarray(nimg).save(make_img_filename(ds), format="png", optimize=True)
+
+
 if __name__ == "__main__":
+    import logging
+    logging.basicConfig(format='%(asctime)s %(message)s', level='INFO')
     from pathlib import Path
+    bucket = 'noaa-goes17'
+    keys = get_s3_keys(bucket, prefix='ABI-L2-MCMIPF/2019/216')
+    for key in keys:
+        logging.info(key)
+        process_s3_file(bucket, key)
+    # goes_dir = Path("/storage/projects/goes_alg/full_disk")
+    # goes_files = sorted(list(goes_dir.glob("*L2-MC*.nc")), reverse=True)
+    # for goes_file in goes_files:
+    #     ds = open_file(goes_file, G17_CORNERS)
+    #     img = make_geocolor_image(ds)
 
-    goes_dir = Path("/storage/projects/goes_alg/goes_data/southwest_adj/")
-    goes_files = sorted(list(goes_dir.glob("*L2-MC*.nc")), reverse=True)
-    for goes_file in goes_files[:10]:
-        ds = open_file(goes_file, G16_CORNERS)
-        img = make_geocolor_image(ds)
-
-        resample_params, shape = make_resample_params(ds, G16_CORNERS)
-        nimg = resample_image(resample_params, shape, img)
-        Image.fromarray(nimg).save(make_img_filename(ds), format="png", optimize=True)
+    #     resample_params, shape = make_resample_params(ds, G17_CORNERS)
+    #     nimg = resample_image(resample_params, shape, img)
+    #     Image.fromarray(nimg).save(make_img_filename(ds), format="png", optimize=True)
